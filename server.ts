dc54e7c9bad35a6b5c6cf2758d0d31d9bc61b260
fengjutian <fjutian@foxmail.com>
2025-08-31 22:24:14 +0800

    feat(server): 更改端口并添加静态文件服务支持
    将服务器端口从3000更改为9527，并添加静态文件服务配置

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ import { commonMiddleware } from "./app/server/db";
 const handler = await fileRouter({ dir: "./app/routes" });
 
 Bun.serve({
-  port: 3000,
+  port: 9527,
   fetch: handler,
   websocket: {
     open: (socket) => {},
@@ -13,6 +13,10 @@ Bun.serve({
     },
     close: (socket, code, reason) => {},
   },
+  static: {
+    directory: new URL("./public", import.meta.url).pathname, // 指定静态文件目录
+    publicPath: "/public",  // URL路径前缀
+  },
 });
 
-console.log("⚡ Bun 扬子鳄全栈模板：http://localhost:3000");
+console.log("⚡ Bun 扬子鳄全栈模板：http://localhost:9527");
